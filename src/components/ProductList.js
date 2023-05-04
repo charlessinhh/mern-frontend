a47bc73f493689aa899de9c9cef70d15fcda5d67
charles-kumar.singh <charles-kumar.singh@atos.net>
2023-05-04 11:57:08 +0530

    version1

---
+++ src/components/ProductList.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    getProducts();
+  });
+
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:5000/products");
+    result = await result.json();
+    setProducts(result);
+  };
+
+  // console.log(products);
+
+  const deleteProduct = async (id) => {
+    console.log(id);
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "delete",
+    });
+    result = await result.json();
+
+    if (result) {
+      alert("product deleted");
+    }
+  };
+
+  return (
+    <div className="product-list">
+      <h3>Product List</h3>
+      <ul>
+        <li>S. No.</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>Category</li>
+        <li>Operation</li>
+      </ul>
+      {products.map((item, index) => (
+        <ul key={item._id}>
+          <li>{index + 1}</li>
+          <li>{item.name}</li>
+          <li>{item.price}</li>
+          <li>{item.category}</li>
+          <li>
+            <button onClick={() => deleteProduct(item._id)}>Delete</button>
+            <Link to={"/update/" + item._id}>Update</Link>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
