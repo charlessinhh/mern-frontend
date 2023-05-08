571b68f734ba1f45fcb01173488e10f9fb583c4a
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-05-08 18:24:53 +0530

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -15,7 +15,7 @@ const SignUp = () => {
 
   const collectData = async () => {
     console.log(name, email, password);
-    let data = await fetch("http://node-api:5000/register", {
+    let data = await fetch("http://localhost:5000/register", {
       method: "post",
       body: JSON.stringify({ name, email, password }),
       headers: {
